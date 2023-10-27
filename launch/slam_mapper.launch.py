982db78da7c4df29f6ec3bc07f70900ffa929cc7
augusto césar do amaral <augustoamaral@estudante.ufscar.br>
2023-10-27 20:43:05 -0300

    fix: slam_mapper rviz arg

--- launch/slam_mapper.launch.py
+++ launch/slam_mapper.launch.py
@@ -6,17 +6,26 @@ from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+	bringup_dir = get_package_share_directory('robovisor')
 
 	use_sim_time = LaunchConfiguration('use_sim_time')
 	use_sim_time_arg = DeclareLaunchArgument('use_sim_time', 
 											default_value='true', 
-											description='Use simulation (Gazebo) clock if true') 
+											description='Use simulation (Gazebo) clock if true')
+	
+	rviz_config_file = LaunchConfiguration('rviz_config_file')
+	rviz_config_file_arg = DeclareLaunchArgument(
+		'rviz_config_file',
+		default_value=os.path.join(bringup_dir, 'rviz', 'nav2.rviz'),
+		description='Full path to the RVIZ config file to use')
 
 	gazebo = IncludeLaunchDescription(
 		PythonLaunchDescriptionSource([FindPackageShare('robovisor'), '/launch', '/gazebo_simulator.launch.py']),
-		launch_arguments={'use_sim_time': use_sim_time}.items()
+		launch_arguments={'use_sim_time': use_sim_time, 'rviz_config_file': rviz_config_file}.items()
 	)
 
 	slam = IncludeLaunchDescription(
@@ -26,6 +35,7 @@ def generate_launch_description():
 
 	return LaunchDescription([
 		use_sim_time_arg,
+		rviz_config_file_arg,
 		gazebo,
 		slam
 	])
